e0e63335194e02e3984333879ef27d5b191ea82a
Aguss261 <261draft@gmail.com>
2023-06-26 08:24:51 -0300

    a

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,15 +3,13 @@ import userRoutes from "../routes/userRoutes.js";
 import peliculasRoutes from "../routes/peliculaRoutes.js";
 import salaRoutes from "../routes/salaRoutes.js";
 import generoRoutes from "../routes/generoRoutes.js";
-import productoRoutes from "../routes/productoRoutes.js";
 
 const indexRoutes = Router();
 
 indexRoutes.use("/users", userRoutes);
 indexRoutes.use("/peliculas", peliculasRoutes);
 indexRoutes.use("/salas", salaRoutes);
-indexRoutes.use("/generos", generoRoutes);
-indexRoutes.use("/productoRoutes", productoRoutes);
+indexRoutes.use("/generos", generoRoutes)
 
 
 export default indexRoutes;
